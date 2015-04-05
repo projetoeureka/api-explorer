ef15f0fb502013e5ed4d102db611716984f90e65
Sherman <sherman@geekie.com.br>
2015-04-05 00:55:12 -0300

    We can haz CORS request!

--- chromeapp/index.js
+++ chromeapp/index.js
@@ -1,35 +1,158 @@
 $(function() {
-    var editor = ace.edit("input-http-editor");
-    editor.setTheme("ace/theme/chrome");
-    editor.setOptions({maxLines: 20});
+  var inputHttpEditor = ace.edit("input-http-editor");
+  inputHttpEditor.setTheme("ace/theme/chrome");
+  inputHttpEditor.setOptions({maxLines: 20});
 
-    editor = ace.edit("input-body-editor");
-    editor.setTheme("ace/theme/chrome");
-    editor.setOptions({maxLines: Infinity});
-    editor.getSession().setMode("ace/mode/json");
-    
-    editor = ace.edit("output-http-editor");
-    editor.setTheme("ace/theme/monokai");
-    editor.setReadOnly(true);
-    editor.setOptions({maxLines: 20, showPrintMargin: false});
-    
-    editor = ace.edit("output-body-editor");
-    editor.setTheme("ace/theme/monokai");
-    editor.setReadOnly(true);
-    editor.setOptions({maxLines: Infinity, showPrintMargin: false});
-    editor.getSession().setUseWorker(false);
-    editor.getSession().setMode("ace/mode/json");
-    
-    $("#input-editor").resizable({
-      handles: "e",
-      autoHide: false,
-      minWidth: 200,
-      maxWidth: 1000,
-      resize: function(event, ui) {
-        var outputEditor = $(this).next();
-        outputEditor.css("left", $(this).outerWidth(true) + "px");
+  var inputBodyEditor = ace.edit("input-body-editor");
+  inputBodyEditor.setTheme("ace/theme/chrome");
+  inputBodyEditor.setOptions({maxLines: 1000});
+  inputBodyEditor.getSession().setMode("ace/mode/json");
+  
+  var outputHttpEditor = ace.edit("output-http-editor");
+  outputHttpEditor.setTheme("ace/theme/monokai");
+  outputHttpEditor.setReadOnly(true);
+  outputHttpEditor.setOptions({maxLines: 20, showPrintMargin: false});
+  
+  var outputBodyEditor = ace.edit("output-body-editor");
+  outputBodyEditor.setTheme("ace/theme/monokai");
+  outputBodyEditor.setReadOnly(true);
+  outputBodyEditor.setOptions({maxLines: Infinity, showPrintMargin: false});
+  outputBodyEditor.getSession().setUseWorker(false);
+  outputBodyEditor.getSession().setMode("ace/mode/json");
+  
+  $("#input-editor").resizable({
+    handles: "e",
+    autoHide: false,
+    minWidth: 200,
+    maxWidth: 1000,
+    resize: function(event, ui) {
+      var outputEditor = $(this).next();
+      outputEditor.css("left", $(this).outerWidth(true) + "px");
+    }
+  });
+  
+  window.serverList.whenServerListAvailable(function(serverList) {
+    var $select = $("#server-host").selectize({
+      valueField: "url",
+      labelField: "url",
+      searchField: "url",
+      options: serverList,
+      render: {
+        item: function(item, escape) {
+          return "<div class='server-item'><span class='api'>" + (
+            item.api ? escape(ServerList.getApiDisplayName(item.api)) :
+            escape("<API desconhecida>")
+          ) + "</span>\n<span class='url'>" + escape(item.url) + "</span></div>";
+        },
+        option: function(item, escape) {
+          return "<div class='server-option'><span class='api'>" + (
+            item.api ? escape(ServerList.getApiDisplayName(item.api)) :
+            escape("<API desconhecida>")
+          ) + "</span>\n<span class='url'>" + escape(item.url) + "</span></div>";
+        }
+      },
+      createFilter: function(url) {
+        info = ServerList.identifyServer(url);
+        if (!info.ok) {
+          return false;
+        }
+       
+        return !window.serverList.alreadyHas(url);
+      },
+      create: function(url) {
+        info = ServerList.identifyServer(url);
+        if (!info.ok) {
+          window.alert("URL inválida");
+        }
+        if (info.signatureMethod == "geekie-sign-v1") {
+          return window.serverList.addServer(new Server({url: url}));
+        } else if (info.signatureMethod == "geekie-sign-v2") {
+          return window.serverList.addServer(new Server({url: url}));
+        } else {
+          window.alert("Tipo de autenticação não suportado por esse app");
+        }
+        return false;
       }
     });
     
-    $("#server-host").selectize();
+    window.serverSelect = $select[0].selectize;
+  });
+  
+  $(".send-request").on("click", function() {
+    var server = window.serverList.getServer(window.serverSelect.getValue());
+    if (!server) {
+      window.alert("Nenhum servidor selecionado");
+    }
+    console.log(server);
+    
+    var requestParser = /(GET|POST|PUT|PATCH|DELETE|HEAD|OPTIONS) (\/.*)/i;
+    var inputHttp = inputHttpEditor.getValue();
+    var inputHttpLines = inputHttp.replace("\r", "").split("\n");
+    if (!inputHttpLines.length) {
+      window.alert("Requisição em branco");
+    }
+    
+    if (!requestParser.exec(inputHttpLines[0])) {
+      window.alert("Requisição inválida\n\nPrimeira linha deve conter algo " +
+                  "como GET /organizations/321313213/who-am-i");
+    }
+    
+    var requestTarget = requestParser.exec(inputHttpLines[0]);
+    
+    var request = {
+      method: requestTarget[1],
+      url: (server.url + (server.url.endsWith("/") ? "": "/") + requestTarget[2].substring(1)),
+      path_qs: requestTarget[2],
+      headers: {}
+    };
+    var invalidHeaders = [];
+    
+    inputHttpLines.slice(1).filter(function (line) {
+      return $.trim(line).length;
+    }).forEach(function (headerLine) {
+      var sep = headerLine.indexOf(":");
+      if (sep <= 0) {
+        return invalidHeaders.push(headerLine);
+      }
+      
+      var headerName = $.trim(headerLine.substring(0, sep));
+      if (!headerName.length) {
+        return invalidHeaders.push(headerLine);
+      }
+      
+      var headerValue = $.trim(headerLine.substring(sep + 1));
+      if (!headerValue.length) {
+        return invalidHeaders.push(headerLine);
+      }
+      
+      request.headers[headerName] = headerValue;
+    });
+    
+    if (invalidHeaders.length) {
+      window.alert("Headers inválidos\n\n" + invalidHeaders.join("\n"));
+    }
+    
+    server.getSigningFunction(request, function(signingFunction) {
+      signingFunction.sign(request);
+      server.send(request, function(response) {
+        var responseHttpInfo = response.statusLine + "\n\n" + response.headers;
+        var limitLines = function(body) {
+          var lines = body.split("\n");
+          var limit = 10000;
+          if (lines.length > limit) {
+            return (lines.slice(0, limit).join("\n") 
+                    + "\n\n(...) output limited to " + limit + " lines");
+          }
+          return body;
+        };
+        outputHttpEditor.setValue(responseHttpInfo, -1);
+        try {
+          var json = JSON.parse(response.body);
+          outputBodyEditor.setValue(limitLines(JSON.stringify(json, null, 4)), -1);
+        } catch(e) {
+          outputBodyEditor.setValue(limitLines(response.body), -1);
+        }
+      });
+    });
+  });
 });
--- chromeapp/serverlist.js
+++ chromeapp/serverlist.js
@@ -1,20 +1,130 @@
 function ServerList(serverList) {
   this._servers = [];
   this._initDone = false;
+  this._initCallbacks = [];
   
   this.init = function() {
-    chrome.storage.sync.get("serverList", function(items) {
+    chrome.storage.sync.get(["serverList"], function(items) {
       if (chrome.runtime.lastError) {
         console.log(chrome.runtime.lastError);
         return;
       }
-      items.servers = items.servers || [];
-      this._servers = items.servers.map(function(server) { 
+      items.serverList = items.serverList || [];
+      this._servers = items.serverList.map(function(server) { 
         return new Server(server); 
       });
+      this._initDone = true;
+      this._initCallbacks.forEach(function(callback) {
+        callback(this._servers);
+      }.bind(this));
+    }.bind(this));
+  };
+  
+  this.whenServerListAvailable = function(callback) {
+    if (this._initDone) {
+      callback(this._servers);
+    } else {
+      this._initCallbacks.push(callback);
+    }
+  };
+  
+  this.addServer = function(server) {
+    this._servers.push(server);
+    chrome.storage.sync.set({"serverList": this._servers.map(function(server) {
+      return server.toJson();
+    })});
+    return server;
+  };
+  
+  this.getServer = function(url) {
+    var servers = this._servers.filter(function(server) {
+      return server.url.startsWith(url) || url.startsWith(server.url);
+    });
+    if (servers.length) {
+      return servers[0];
+    }
+    return null;
+  };
+  
+  this.alreadyHas = function(url) {
+    return !!this.getServer(url); 
+  };
+}
+
+  
+ServerList.identifyServer = function(serverUri) {
+  if (!serverUri.endsWith("/")) {
+    serverUri += "/";
+  }
+  
+  var validPattern = /https?:\/\/[a-z-.]+(:\d{2,5})?\/(api\/(v\d\/)?)?/;
+  if (!validPattern.test(serverUri)) {
+    return {ok: false};
+  }
+  
+  externalApiUris = [
+    "http://api.geekielab.com.br/",
+    "http://geekielab-api.herokuapp.com/",
+    "http://geekielab-api-canary.herokuapp.com/",
+    "http://geekielab-api-staging.herokuapp.com/",
+    "http://geekielab-api-latest.herokuapp.com/",
+  ];
+  
+  swagUris = [
+    "http://www.geekielab.com.br/",
+    "http://geekieid.herokuapp.com/",
+    "http://geekieid-staging.herokuapp.com/",
+    "http://geekieid-canary.herokuapp.com/",
+    "http://geekieid-latest.herokuapp.com/"
+  ];
+  
+  if (externalApiUris.indexOf(serverUri) >= 0) {
+    return {ok: true, signatureMethod: "geekie-sign-v2", api: "extapi"};
+  } else if (swagUris.indexOf(serverUri) >= 0) {
+    return {ok: true, signatureMethod: "geekie-sign-v1", api: "swag"};
+  } else {
+    return {ok: true, signatureMethod: "geekie-sign-v1", api: undefined};
+  }
+};
+
+ServerList.getApiDisplayName = function(api) {
+  return {extapi: "API GeekieLab", swag: "swag"}[api];
+};
+
+function Server(options) {
+  var info = ServerList.identifyServer(options.url);
+  
+  this.url = options.url;
+  this.apiKeys = options.apiKeys || [];
+  
+  this.api = info.api;
+  this.signatureMethod = info.signatureMethod;
+  
+  this.toJson = function() {
+    return {url: this.url, apiKeys: this.apiKeys};
+  };
+
+  this.getSigningFunction = function(request, callback) {
+    callback({
+      sign: function(request) {}
+    });
+  };
+  
+  this.send = function(request, callback) {
+    $.ajax({
+      type: request.method,
+      headers: request.headers,
+      url: request.url,
+      complete: function(jqXhr) {
+        callback({
+          statusLine: "HTTP " + jqXhr.status + " " + jqXhr.statusText,
+          headers: jqXhr.getAllResponseHeaders(),
+          body: jqXhr.responseText
+        });
+      }
     });
   };
 }
 
-
 var serverList = new ServerList();
+serverList.init();
